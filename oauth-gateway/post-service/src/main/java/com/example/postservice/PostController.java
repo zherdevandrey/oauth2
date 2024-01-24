df0bf41b81f1a100a3bf158e1f4e7602a24ba524
user <user@mail.ru>
2024-01-24 08:20:14 +0300

    commit

---
+++ oauth-gateway/post-service/src/main/java/com/example/postservice/PostController.java
@@ -0,0 +1,20 @@
+package com.example.postservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+public class PostController {
+
+    @GetMapping("/api/posts")
+    public List<Post> posts() {
+        log.info("Get posts request");
+        var post = new Post(UUID.randomUUID(), "post name");
+        return List.of(post);
+    }
+}
