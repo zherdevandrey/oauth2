52257f8602edd2a260635b443d02d06a893aaa0f
mrzhe <mr.zherdev1992@gmail.com>
2024-02-06 09:43:30 +0300

    add blitz

---
+++ blitz/src/main/java/com/example/blitz/BlitzApplication.java
@@ -0,0 +1,13 @@
+package com.example.blitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlitzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlitzApplication.class, args);
+    }
+
+}
